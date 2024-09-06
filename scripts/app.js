5eb51b5013b08b4e78eebde95be4d864a9b96ab8
hamid <hamid.nikoofard.72@gmail.com>
2024-09-06 23:42:24 +0330

    fixed footer and contact us

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,52 @@ BTNEL.forEach((BTN, index) => {
     answer[index].classList.toggle('answer');
   });
 });
+
+const boxCommentEL = document.querySelector(".box-comment")
+
+const getDataComment = async () => {
+    try {
+        const response = await fetch("https://dummyjson.com/comments");
+        const comments = await response.json();
+        return comments; 
+    } catch (error) {
+        console.error('Error fetching comments:', error);
+    }
+}
+
+getDataComment().then((comments) => {
+    let commentHTML = '';
+    comments.comments.slice(0, 4).forEach((item) => {
+        commentHTML += 
+        `
+            <div class="info-comment">
+                <div class="comment">
+                    <p>${item.body}</p> 
+                </div>
+                <div class="info">
+                    <span>${item.user.username}</span>
+                    <p>${item.user.fullName}</p> 
+                </div>
+            </div>
+        `;
+
+        boxCommentEL.innerHTML += commentHTML
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:50,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+});
